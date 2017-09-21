913e22b3998409931b7e3fcf6749cf4a3d9202cc
Maximilian Pfister <maximilian.pfister90@gmail.com>
2017-09-21 11:38:18 +0200

    First commit
    First commit

---
+++ src/gekko/exchanges/BitfinexArbitrageExchange.java
@@ -0,0 +1,25 @@
+package gekko.exchanges;
+
+import java.io.IOException;
+
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.bitfinex.v1.BitfinexExchange;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.exceptions.ExchangeException;
+import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
+import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
+
+public class BitfinexArbitrageExchange extends AbstractArbitrageExchange{
+	
+	public BitfinexArbitrageExchange(CurrencyPair currencyPair, String apiKey, String secretKey){
+		super(BitfinexExchange.class, "Bitfinex",  currencyPair, apiKey, secretKey);
+	}
+	
+	@Override
+	public void checkBalances() throws NotAvailableFromExchangeException, NotYetImplementedForExchangeException, ExchangeException, IOException{
+	    baseAmount = accountService.getAccountInfo().getWallet().getBalance(currencyPair.base).getTotal().doubleValue();
+	    counterAmount = accountService.getAccountInfo().getWallet().getBalance(currencyPair.counter).getTotal().doubleValue();
+	}
+
+}
